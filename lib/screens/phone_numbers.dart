6f09625d14c6e061db394a3552ad2c02f053f7bc
shivam gupta <shivay7837@gmail.com>
2022-07-22 20:53:18 +0530

    ask permission updated

--- lib/screens/phone_numbers.dart
+++ lib/screens/phone_numbers.dart
@@ -1,5 +1,5 @@
-import 'package:contacts/Widget/phoneNumbers.dart';
 import 'package:contacts/Widget/drawer.dart';
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 import 'package:contacts/Widget/search_bar_widget.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -13,10 +13,27 @@ class PhoneNumbers extends StatefulWidget {
 }
 
 class _PhoneNumbersState extends State<PhoneNumbers> {
+bool isMounted = false;
+  Iterable<Contact>? _contacts;
 
+  @override
+  void initState() {
+    getContacts();
+    super.initState();
+  }
+
+  Future<void> getContacts() async {
+    //Make sure we already have permissions for contacts when we get to this
+    //page, so we can just retrieve it
+    final Iterable<Contact> contacts = await ContactsService.getContacts();
+    setState(() {
+      _contacts = contacts;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       drawer: NavBar(),
       floatingActionButton: SizedBox(
         height: 64.0,
@@ -67,30 +84,29 @@ class _PhoneNumbersState extends State<PhoneNumbers> {
               ),
             ],
           ),
-          const SizedBox(height: 10),
+          const SizedBox(height: 5),
           const Divider(
             color: Colors.black,
             thickness: 0.5,
           ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                _getPermission();
+          GestureDetector(
+             onTap:(){
+                _askForPermission();
               },
-              child: const Text('See Contacts'),
-            ),
+            child: _renderContacts(contacts: _contacts),
           )
         ],
       ),
     );
   }
 
-  void _getPermission() async {
+  void _askForPermission() async {
     //status can either denied,restricted,granted or permanent denied
     final status = await Permission.contacts.status;
     //we alredy give permission
     if (status.isGranted) {
-     Navigator.push(context, MaterialPageRoute(builder: (context)=>ContactDetails()));
+      if(isMounted){}
+     Navigator.push(context, MaterialPageRoute(builder: (context)=>_renderContacts(contacts: null,)));
     } else if (status.isDenied) {
       //we didn't  ask for permission aready or we denied
      print(" permission denied");
@@ -98,5 +114,49 @@ class _PhoneNumbersState extends State<PhoneNumbers> {
     if (await Permission.contacts.request().isGranted) {
       print("permision was granted");
     }
+  }
+}
+
+class _renderContacts extends StatelessWidget {
+  const _renderContacts({
+    Key? key,
+    required Iterable<Contact>? contacts,
+  }) : _contacts = contacts;
+
+  final Iterable<Contact>? _contacts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10),
+      height:589,
+      color: Colors.white,
+      child: _contacts != null
+          //Build a list view of all contacts, displaying their avatar and
+          // display name
+          ? ListView.builder(
+            padding: EdgeInsets.only(top: 0),
+      itemCount: _contacts?.length ?? 0,
+      itemBuilder: (BuildContext context, int index) {
+        Contact? contact = _contacts?.elementAt(index);
+        return ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric( horizontal: 18),
+          leading: (contact!.avatar != null && contact.avatar!.isNotEmpty)
+              ? CircleAvatar(
+                )
+              : CircleAvatar(
+                maxRadius: 25,
+                  child: Text(contact.initials()),
+                ),
+          title: Text(contact.displayName ?? '',style: TextStyle(fontSize: 18),),
+          // subtitle: Text(number),
+          //This can be further expanded to showing contacts detail
+          // onPressed().
+        );
+      },
+    )
+          : const Center(child: CircularProgressIndicator()),
+    );
   }
 }
